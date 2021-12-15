d12c408d299250e7a13d58c26f8f5fa873d5714f
minjyo <minjyo8823@gmail.com>
2021-12-15 14:46:38 +0900

    init: setting mysql, sequelize

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 
+const { sequelize } = require("./models");
+
 dotenv.config();
 const indexRouter = require("./routes");
 const userRouter = require("./routes/user");
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
+
+sequelize
+    .sync({ force: false })
+    .then(() => {
+        console.log("데이터베이스 연결 성공");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
